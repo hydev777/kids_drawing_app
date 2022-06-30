c23d5c90b2654c73e4d35ee6432b56d448d4f446
hydev777 <hytechwilson@gmail.com>
2022-06-30 16:48:38 -0400

    update

--- lib/screens/drawer_panel/board/board.dart
+++ lib/screens/drawer_panel/board/board.dart
@@ -25,25 +25,56 @@ class Board extends CustomPainter {
 
     for (int i = 0; i < (points!.length - 1); i++) {
 
-      if (points![i].point != null && points![i + 1].point != null) {
-        canvas.drawLine(
-          points![i].point!,
-          points![i + 1].point!,
-          Paint()
-            ..color = points![i].color!
-            ..strokeWidth = points![i].size!
-            ..strokeJoin = StrokeJoin.miter,
-        );
-      } else {
-        canvas.drawCircle(
-          points![i - 1].point!,
-          Paint().strokeWidth / 2,
-          Paint()
-            ..color = points![i - 1].color!
-            ..strokeWidth = points![i - 1].size!
-            ..strokeJoin = StrokeJoin.miter,
-        );
+      if(points![i].tool == Tools.pencil) {
+
+        if (points![i].point != null && points![i + 1].point != null) {
+          canvas.drawLine(
+            points![i].point!,
+            points![i + 1].point!,
+            Paint()
+              ..color = points![i].color!
+              ..strokeWidth = points![i].size!
+              ..strokeJoin = StrokeJoin.miter,
+          );
+        }
+        else if(points![i].point == null) {
+          canvas.drawCircle(
+            points![i - 1].point!,
+            points![i - 1].size! / 2,
+            Paint()
+              ..color = points![i - 1].color!
+              ..strokeWidth = points![i - 1].size!
+              ..strokeJoin = StrokeJoin.miter,
+          );
+        }
+
+      } else if (points![i].tool == Tools.eraser) {
+
+        if (points![i].point != null && points![i + 1].point != null) {
+          canvas.drawLine(
+            points![i].point!,
+            points![i + 1].point!,
+            Paint()
+              ..color = backgroundColor!
+              ..strokeWidth = points![i].size!
+              ..strokeJoin = StrokeJoin.miter,
+          );
+        }
+        else if(points![i].point == null) {
+          canvas.drawCircle(
+            points![i - 1].point!,
+            points![i - 1].size! / 2,
+            Paint()
+              ..color = backgroundColor!
+              ..strokeWidth = points![i - 1].size!
+              ..strokeJoin = StrokeJoin.miter,
+          );
+        }
+
       }
+
+
+
     }
 
   }
