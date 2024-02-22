ca3255ec532adc85cd2d51dfd71af217f9af2be3
hydev777 <hytechwilson@gmail.com>
2024-02-22 15:58:37 -0400

    added provider wrapper to provider

--- lib/provider_wrapper/provider_wrapper.dart
+++ lib/provider_wrapper/provider_wrapper.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../screens/drawer_panel/provider/provider.dart';
 
 class ProviderWrapper extends StatelessWidget {
-  const ProviderWrapper({Key? key}) : super(key: key);
+  const ProviderWrapper({
+    Key? key,
+    this.child,
+  }) : super(key: key);
+
+  final Widget? child;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (ctx) => DrawerProvider(),
+        )
+      ],
+      child: child,
+    );
   }
 }
--- lib/screens/kids_drawing_app/kids_drawing_app.dart
+++ lib/screens/kids_drawing_app/kids_drawing_app.dart
@@ -1,20 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
+import '../../provider_wrapper/provider_wrapper.dart';
 import '../../router/router.dart';
-import '../drawer_panel/provider/provider.dart';
 
 class KidsDrawingApp extends StatelessWidget {
   const KidsDrawingApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (ctx) => DrawerProvider(),
-        )
-      ],
+    return ProviderWrapper(
       child: MaterialApp.router(
         routerConfig: router,
         debugShowCheckedModeBanner: false,
