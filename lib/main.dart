f298c7e61efeabbb6c4bfe5b489b103a3f28063b
hydev777 <hytechwilson@gmail.com>
2024-03-16 13:40:06 -0400

    feat(app): added splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'screens/kids_drawing_app/kids_drawing_app.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+  FlutterNativeSplash.remove();
   runApp(const KidsDrawingApp());
 }
